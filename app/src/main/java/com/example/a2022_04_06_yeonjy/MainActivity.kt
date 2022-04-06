542af7d346148d201eeb0d94cb14bf76f70090a2
yeonjy <yeonjypaper@gmail.com>
2022-04-06 23:13:00 +0900

    Initial commit

---
+++ app/src/main/java/com/example/a2022_04_06_yeonjy/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.example.a2022_04_06_yeonjy
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        var name = findViewById<TextView>(R.id.txtName)
+
+        name.text = "LimYeonjy"      // name의 text로 들어감
+
+        // 더해질 숫자
+        var num = 0
+        var score = findViewById<TextView>(R.id.score)
+        score.text = num.toString()
+
+        val btn = findViewById<Button>(R.id.btnPlus)
+
+        btn.setOnClickListener {
+            num ++
+            score.text = num.toString()
+        }
+    }
+
+
+
+}
